98ea39d89219708e589c16736b0022647207345a
osawa-koki <github@osawa.dev>
2023-02-27 01:18:16 +0900

    🦤🦤🦤

--- setting.ts
+++ setting.ts
@@ -4,7 +4,7 @@ const Env = require('./next.config.js');
 const isProd = process.env.NODE_ENV === 'production';
 
 const Setting = {
-  title: '🦖 simple-percent_encode 🦖',
+  title: '🦤 simple-percent_encode 🦤',
   isProd,
   basePath: Env.basePath,
   timeout: 3000,
