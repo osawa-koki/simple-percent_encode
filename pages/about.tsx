f8388fb272c8869139771448453833ad94938437
osawa-koki <github@osawa.dev>
2023-02-27 01:28:26 +0900

    説明書いた。

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,24 +2,24 @@ import React from "react";
 
 import Layout from "../components/Layout";
 
-const base64_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split('');
+const letters_need_to_percent_encode = ':/?#[]@!$&\'()*+,;='.split('');
 
 export default function AbutPage() {
   return (
     <Layout>
       <div id="About">
-        <h1>What is BASE64?</h1>
-        <p className="mt-3">BASE64とはバイナリデータ(テキストデータを含むすべてのデータ)を一定の規則で、以下の64種の半角英数字と一部記号に変換した表記のことを言います。</p>
-        <div id="Base64Letters">
-          {
-            base64_letters.map((letter, _) => {
-              return (
-                <div key={letter}>{letter}</div>
-              );
-            })
-          }
+        <h1>What is Percent(URL) Encode?</h1>
+        <p className="mt-3">パーセント(URL)エンコードとは、URLとして使用できない文字を、一定の規則に基づいて`%`を用いた別の文字に変換することを言います。<br /><br />パーセントを用いたエンコード手法であるためパーセントエンコードと呼ばれますが、特にURLで用いられることからURLエンコードとも呼ばれます。<br /><br />具体的には以下の文字をURLに含む場合にパーセントエンコードする必要があります。</p>
+        <div id="PercentEncodeLetters">
+        {
+          letters_need_to_percent_encode.map((letter, _) => {
+            return (
+              <div key={letter} className="mt-3">{letter}</div>
+            );
+          })
+        }
         </div>
-        <p className="mt-3">これによって画像や動画などのデータを文字列として扱うことができるようになります。<br /><br />しかしながら、バイナリを文字に変換するため、データ量が大きくなるという問題があります。<br />データ量はおおよそ、1.3倍になります。</p>
+        <p className="mt-3">上記の文字以外でもパーセントエンコードをすることが可能です。</p>
       </div>
     </Layout>
   );
